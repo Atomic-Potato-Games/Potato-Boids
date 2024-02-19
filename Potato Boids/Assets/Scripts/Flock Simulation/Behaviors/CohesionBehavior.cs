9057a66530aae68a3efdd68f27c88d9c84cb43b2
Atomic Potato <medyanmehiddine@gmail.com>
2024-02-19 17:31:22 +0200

    Cohesion Behavior

---
+++ Potato Boids/Assets/Scripts/Flock Simulation/Behaviors/CohesionBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlockSimulation
+{
+    [CreateAssetMenu (fileName = "CohesionBehavior", menuName = "Flock/Behavior/Cohesion")]
+    public class CohesionBehavior : FlockBehavior
+    {
+        public override Vector3 CalculateMoveVector(Agent agent, List<Transform> neighbors, Flock flock)
+        {
+            bool isNeighborsExist = neighbors.Count != 0;
+            if (!isNeighborsExist)
+                return Vector3.zero;
+
+            Vector3 avgPosition = GetNeighborsAvgPosition();
+            // return the avg position offset by the agent position
+            return avgPosition - agent.transform.position;
+
+            Vector3 GetNeighborsAvgPosition()
+            {
+                Vector3 sum = Vector3.zero;
+                foreach (Transform neighbor in neighbors)
+                    sum += neighbor.position;
+                return sum / neighbors.Count;
+            }
+        }
+    }
+}
